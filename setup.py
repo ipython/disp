eac7ada371c3d1c4256152212931c2035abcacc6
Kyle Kelley <rgbkrk@gmail.com>
2017-05-30 15:29:44 -0700

    Basic start to disp

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='disp',
+      version='0.0.0',
+      description='Providing default representations of common objects in Python land',
+      url='http://github.com/ipython/disp',
+      author='IPython developers',
+      author_email='ipython-dev@python.org',
+      license='BSD',
+      packages=['disp'],
+      zip_safe=False)
