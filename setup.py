3bedede930213e94e75cbeb098f407c980ae9db1
Matthias Bussonnier <bussonniermatthias@gmail.com>
2017-06-07 17:21:29 -0700

    bump to version 0.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='disp',
-      version='0.0.0',
+      version='0.0.1',
       description='Providing default representations of common objects in Python land',
       url='http://github.com/ipython/disp',
       author='IPython developers',
