12bc45393a8188d0336d2093f22191e833ad3d58
Matthias Bussonnier <bussonniermatthias@gmail.com>
2017-06-19 10:39:51 -0700

    cleanup

--- disp/py3only.py
+++ disp/py3only.py
@@ -1,29 +1,42 @@
+"""
+This submodule contains formatting utilities and formatters which will work only
+on Python 3.6+. There is no inherent reasons why it would not work on earlier
+version of Python, it just makes use of features that are 3.6 only – Like
+f-strings – to make the code more readable. Feel free to send patches that makes
+it compatible with earlier versions of python
+"""
+
+
 from html import escape
 from typing import List
-import typing
-from IPython.display import HTML, DisplayObject
+from IPython.display import HTML
+from .vendor import get_repr_mimebundle
+
 
 text_formatter = get_ipython().display_formatter.formatters['text/plain']
 
-from there import print
-
-
-try:
-    from IPython.display import get_repr_mimebundle
-except ImportError:
-
-    # poor man's version don't protect against recursion
-    get_repr_mimebundle =  get_ipython().display_formatter.format 
-
 
 def repr(o):
-    return get_repr_mimebundle(o, include='text/plain').data.get('text/plain')
+    """
+    Alternative implementation of repr, whcih goes through IPython display system from plain-text. 
+
+    We go directly though the formatter to avoid recursion complication with
+    get_mime_types, ans for a tiny bit more speed.
+
+    If the real repr is needed, then one need to use builtins.repr
+    """
+    return text_formatter(o)
+
+
+# This is the CSS we want to inject before each top-level object. We should
+# (try-to) make it to work with most frontend, as not all frontends do support
+# CSS injection, let's try to not rely on too much customisation
+
 
 thecss = """
 dl.jupyter-inner-mapping-repr {
     padding-left: 1em;
     margin-bottom: 0;
-
 }
 
 dl.jupyter-inner-mapping-repr > dd {
@@ -47,7 +60,6 @@ ul.jupyter-flat-container-repr > details {
     margin-left: -1em;
 }
 
-
 ul.jupyter-flat-container-repr li{
     padding-left:2em;
     list-style-type: none;
@@ -57,12 +69,10 @@ summary > pre {
     display: inline
 }
 
-
 ul.jupyter-flat-container-repr summary {
     margin-left: 0em;
     display: inline-block;
 }
-
 
 details[open] {
     /*display: inline-block;*/
@@ -86,11 +96,11 @@ details[open] {
     /*display: block;*/
 }
 
-details[open] ~ .xx {
+details[open] ~ .jupyter-breaking-placeholder {
     display: block;
 }
 
-details ~ .xx {
+details ~ .jupyter-breaking-placeholder {
     display: inline;
 }
 .output_subarea > ul.jupyter-flat-container-repr, .output_subarea > ul.jupyter-flat-container-repr > p {
@@ -98,42 +108,46 @@ details ~ .xx {
 }
 """
 
+
+#################################################################################
+#                    Utilities                                                  #
+#################################################################################
+
 def safe(obj):
+    """
+    Given an object (str, or html), return an HTML version. 
+
+    That is to say, if Object is already an HTML object, return it. If it's a string, escape it. 
+
+    """
     if isinstance(obj, HTML):
         return obj
     else:
-        return HTML(escaped_str_or_html(obj))
-    
+        return HTML(htmlify_repr(obj))
 
-def escaped_str_or_html(obj)-> str:
+
+def htmlify_repr(obj)-> str:
     """
     Return a string which is safe to embed in html. 
-    
+
     ie, if obj define rerp_html, return this, otherwise escape its text_repr
     """
-    return get_repr_mimebundle(obj).data.get('text/html', None) or escape(get_repr_mimebundle(obj).data.get('text/plain', ''))
+    return get_repr_mimebundle(obj).data.get('text/html', None) or\
+        escape(repr(obj))
 
 
-# we won't register containers (for now), as we need to inject CSS on the page,
-# and we don't want o inject it on all the sub-reprs.
-# htmlf.for_type(list , lambda t:html_flat_container(t, delims='[]', empty='[]'     ))
-# htmlf.for_type(tuple, lambda t:html_flat_container(t, delims='()', empty='()'     ))
-# htmlf.for_type(set  , lambda t:html_flat_container(t, delims='{}', empty='set({})'))
+def html_flat_container(container: List, delims, empty) -> str:
+    """Retrun an Html representation of a list with recursive HTML repr for all sub objects. 
 
-def html_flat_container(whatever:List, delims, empty) -> str:
-    """
-    retrun an Html representation of a list with recursive HTML repr for all sub objects. 
-    
     If an object does not define an html representation, fallback on plain-text.
-    
-    TODO: Fallback on pretty ?
+
     """
-    if not whatever:
+    if not container:
         return empty
     x = []
-    for index, elem in enumerate(whatever):
-        last = (index == len(whatever)-1)
-        rpr = escaped_str_or_html(elem)
+    for index, elem in enumerate(container):
+        last = (index == len(container) - 1)
+        rpr = htmlify_repr(elem)
         pc = '<span class="post-comma">,</span>' if not last else ''
         pl = '<br/>' if last else ''
         x.append('<li>{}{}</li>'.format(rpr, pc))
@@ -142,62 +156,67 @@ def html_flat_container(whatever:List, delims, empty) -> str:
                         <summary>{delims[0]}</summary>
                         {''.join(x)}
                     </details>
-                    <span class='xx'></span><p>{delims[1]}</p>
+                    <span class='jupyter-breaking-placeholder'></span><p>{delims[1]}</p>
                 </ul>
 
-            """ 
-
-# htmlf.for_type(list , lambda t:html_flat_container(t, delims='[]', empty='[]'     ))
-# htmlf.for_type(tuple, lambda t:html_flat_container(t, delims='()', empty='()'     ))
-# htmlf.for_type(set  , lambda t:html_flat_container(t, delims='{}', empty='set({})'))
-
-html_formatter_for_list  = lambda t:html_flat_container(t, delims='[]', empty='[]'     )
-html_formatter_for_tuple = lambda t:html_flat_container(t, delims='()', empty='()'     )
-html_formatter_for_set   = lambda t:html_flat_container(t, delims='{}', empty='set({})')
+            """
 
 
-def _inner_html_formatter_for_mapping(whatever):
+def html_formatter_for_list(t): return html_flat_container(
+    t, delims='[]', empty='[]')
+
+
+def html_formatter_for_tuple(t): return html_flat_container(
+    t, delims='()', empty='()')
+
+
+def html_formatter_for_set(t): return html_flat_container(
+    t, delims='{}', empty='set({})')
+
+
+def _inner_html_formatter_for_mapping(mapping):
     x = []
-    for key,elem in whatever.items():
+    for key, elem in mapping.items():
         mimebundle = get_repr_mimebundle(elem).data
-        rpr = mimebundle.get('text/html', mimebundle.get('text/html', None)) or escape(repr(elem))
+        representation = mimebundle.get(
+            'text/html', mimebundle.get('text/html', None)) or escape(repr(elem))
         x.append(f"""<dl class='jupyter-inner-mapping-repr'>
                         <dt><b>{escape(str(key))}:</b></dt>
-                        <dd>{rpr}</dd>
+                        <dd>{representation}</dd>
                     </dl>
                   """)
     return ''.join(x)
-        
 
-def html_formatter_for_mapping(whatever, *, open=True):
-    if not whatever :
+
+def html_formatter_for_mapping(mapping, *, open=True):
+    if not mapping:
         return 'dict({})'
     delims = '{}'
     op = 'open' if open else ''
     return f"""
-                <details {op}>
-                    <summary>{delims[0]}</summary>
-                    {_inner_html_formatter_for_mapping(whatever)}
-                </details>
-                {delims[1]}
+            <details {op}>
+                <summary>{delims[0]}</summary>
+                {_inner_html_formatter_for_mapping(mapping)}
+            </details>
+            {delims[1]}
             """
 
-html_formatter_for_dict = html_formatter_for_mapping
 
-# Same thing we don't register dict, but we'll use the code in here
-# htmlf.for_type(dict, html_dict)
+html_formatter_for_dict = html_formatter_for_mapping
 
 
 def html_formatter_for_Response(req):
     import json
     attrs = None
-    def in_f(k,v):
+
+    def in_f(k, v):
         if k == 'headers':
             return HTML(html_formatter_for_mapping(v, open=False))
         else:
             return v
 
-    attrs = _inner_html_formatter_for_mapping({k:in_f(k,v) for (k,v) in vars(req).items() if not k.startswith('_')})
+    attrs = _inner_html_formatter_for_mapping(
+        {k: in_f(k, v) for (k, v) in vars(req).items() if not k.startswith('_')})
     try:
         json_content = req.json()
 
@@ -231,16 +250,19 @@ def details(summary, details_):
         rdetails = safe(details_)._repr_html_()
         return HTML(f"<details><summary>{rsum}</summary>{rdetails}</details>")
     else:
-        rsum = escaped_str_or_html(summary)
+        rsum = htmlify_repr(summary)
         return HTML(f"{rsum}")
-    
+
+
 def pre(string):
     assert isinstance(string, str)
     return HTML(f"<pre>{escape(string)}</pre>")
 
+
 def well(s):
     s = safe(s)._repr_html_()
-    return HTML('<div class="well">'+s+'</div>')
+    return HTML('<div class="well">' + s + '</div>')
+
 
 def gen_help(obj):
     doc = next(filter(None, (x.__doc__ for x in type(obj).mro())))
@@ -248,44 +270,36 @@ def gen_help(obj):
     <pre title='{escape(doc)}'>{escape(repr(obj))}<pre>
     """
 
+
 def general_repr(obj):
-    return f'<details><summary><pre>{escape(repr(obj))}<pre></summary>'+\
-        _inner_html_formatter_for_mapping({k:v for (k,v) in vars(obj).items() if not k.startswith('_')}) +'</details>'
-
-def ellide(s):
-    return s if (len(s) < 75) else s[0:50]+'...'+s[-16:]
-
-def _print_bytestr(arg, p, cycle):
-    p.text('<bytes '+ellide(repr(arg))+' at {}>'.format(hex(id(arg))))       
-
-    text_formatter.for_type(bytes, _print_bytestr)
-
-    
+    return f'<style>{thecss}</style>' +\
+           f'<details><summary><pre>{escape(repr(obj))}<pre></summary>' +\
+           _inner_html_formatter_for_mapping({k: v for (k, v) in vars(obj).items() if not k.startswith('_')}) +\
+        '</details>'
 
 
 def html_formatter_for_type(obj):
     try:
-        mro = obj.mro() #[o for o in  if o is not object]
+        mro = obj.mro()  # [o for o in  if o is not object]
     except TypeError:
         mro = ()
     if len(mro) > 1:
         mime = get_repr_mimebundle(mro[1], include='text/html').data
         return f'<details><summary><pre>{escape(repr(obj))}</pre></summary>'\
-                + well(HTML(f"""
+            + well(HTML(f"""
                 <p><pre alpha>{obj.__doc__ or ''}</pre></p>
                 <p> Inherit from :</p>
                 <ul>
                   <li>{mime.get('text/html')}</li>
                 </ul>"""))._repr_html_()\
-                +'</details>'
+            + '</details>'
     else:
-        return f'<style>{thecss}</style>'+f'<pre>{escape(repr(obj))}</pre>'
-    
+        return f'<style>{thecss}</style>' + f'<pre>{escape(repr(obj))}</pre>'
 
 
 def html_formatter_for_builtin_function_or_method(obj):
     ip = get_ipython()
-    res = {k:v for (k,v) in ip.inspector.info(obj).items() if v}
+    res = {k: v for (k, v) in ip.inspector.info(obj).items() if v}
     docstring = res.get('docstring')
     res.pop('found')
     res.pop('string_form')
@@ -296,9 +310,9 @@ def html_formatter_for_builtin_function_or_method(obj):
         res['docstring'] = pre(docstring)
     else:
         del res['docstring']
-    #return repr(details(repr(obj), escaped_str_or_html(res)))
-    return f'<style>{thecss}</style>'+escaped_str_or_html(details(HTML(escape(repr(obj))), HTML('<div class="well">'+_inner_html_formatter_for_mapping(res)+'<div>')))
+    # return repr(details(repr(obj), htmlify_repr(res)))
+    return f'<style>{thecss}</style>' + htmlify_repr(details(HTML(escape(repr(obj))), HTML('<div class="well">' + _inner_html_formatter_for_mapping(res) + '<div>')))
 
-    
+
 def html_formatter_for_module(obj):
     return f'<style>{thecss}</style>' + details(HTML(escape(repr(obj))), well(pre(obj.__doc__ or '')))._repr_html_()
